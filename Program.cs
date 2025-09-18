e0298332bb40654843603c413766893d6f3c15a3
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:54:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using HerancaPolimorfismo6.Entities;
+
+namespace HerancaPolimorfismo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPayer> list = new List<TaxPayer>();
+            Console.Write("Enter the number of tax payers: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or Company (i/c)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            double sum = 0.0;
+            foreach (TaxPayer tp in list)
+            {
+                Console.WriteLine(tp.Name + ": $" + tp.Tax().ToString("F2", CultureInfo.InvariantCulture));
+                sum += tp.Tax();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
